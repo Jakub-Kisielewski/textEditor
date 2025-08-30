a36a0b1200344a619d9f2b8444a23e9a859f7d89
Jakub Kisielewski <186347180+Jakub-Kisielewski@users.noreply.github.com>
2025-08-30 15:30:31 +0100

    initial commit

---
+++ kilo.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <termios.h> 
+
+struct termios origTermios;
+
+void disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &origTermios);
+}
+
+void enableRawMode() {
+  tcgetattr(STDIN_FILENO, &origTermios);
+  atexit(disableRawMode);
+
+  struct termios raw = origTermios;
+  raw.c_lflag &= ~(ECHO | ICANON);
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main() {
+  enableRawMode();
+
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (iscntrl(c)) {
+      printf("%d\n", c);
+    } else {
+      printf("%d ('%c')\n", c, c);
+    }
+  }
+  return 0;
+}
