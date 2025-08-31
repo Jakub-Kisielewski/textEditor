a71c9ab9bb2d7a5e8e1da25d63cdb20f522edef0
Jakub Kisielewski <186347180+Jakub-Kisielewski@users.noreply.github.com>
2025-08-31 14:28:12 +0100

    clear screen on exit

--- kilo.c
+++ kilo.c
@@ -17,6 +17,9 @@ struct termios origTermios;
 /*** terminal ***/
 
 void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
   perror(s);
   exit(1);
 }
@@ -53,6 +56,7 @@ char editorReadKey() {
 
 void editorRefreshScreen() {
   write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
 /*** input ***/ 
@@ -62,6 +66,8 @@ void editorProcessKeypress() {
 
   switch (c) {
     case CTRL_KEY('q'):
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
   }
